9a6bc32e43b9c86dcb573f442cf56301aa1d0d75
Aishuvthija <168397436+Aishuvthija@users.noreply.github.com>
2025-05-19 22:36:51 +0530

    Update welcome_app.py

--- welcome_app.py
+++ welcome_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Python-Flask!"
+    return "Hello from Python-Flask automated successfully!"
 
 @app.route('/health')
 def health():
