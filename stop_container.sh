28e201755af3c84cd20d4c877d412fac8db668d6
Aishuvthija <168397436+Aishuvthija@users.noreply.github.com>
2025-05-22 21:45:18 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-ContainerID=$(docker ps | awk 'NR>1 {print $1}')
-docker rm -f $ContainerID
+
+# Get running container IDs (skip header)
+ContainerID=$(docker ps -q)
+
+# If any container is running, stop it
+if [ ! -z "$ContainerID" ]; then
+  docker rm -f $ContainerID
+fi
