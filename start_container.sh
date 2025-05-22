67493af8bfefce2a1691c3bee1e91313b725a1d2
Aishuvthija <168397436+Aishuvthija@users.noreply.github.com>
2025-05-22 22:44:54 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -e
 
-IMAGE_NAME="aishuvthija/simple-python-flask-app"
-CONTAINER_NAME="python-app"
+# Pull the Docker image from Docker Hub
+docker pull aishuvthija/simple-python-flask-app
 
-# Pull the latest Docker image
-docker pull $IMAGE_NAME
-
-# Check if container is already running and remove it to avoid conflicts
-if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-    echo "Stopping running container $CONTAINER_NAME..."
-    docker stop $CONTAINER_NAME
-    docker rm $CONTAINER_NAME
-fi
-
-# Run the Docker container
-docker run -d --name $CONTAINER_NAME -p 5000:5000 $IMAGE_NAME
-
+# Run the Docker image as a container
+docker run -d -p 5000:5000 aishuvthija/simple-python-flask-app
